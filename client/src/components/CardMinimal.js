8bc27651a3866e0410132292a8de0dac8d7f1ee2
Nisha Pant <nishap2@illinois.edu>
2020-05-14 00:05:52 -0500

    got it working with classes and state

--- client/src/components/CardMinimal.js
+++ client/src/components/CardMinimal.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect } from 'react';
-import {loadStripe} from '@stripe/stripe-js';
-import {CardElement, Elements, useElements, useStripe, Stripe} from '@stripe/react-stripe-js';
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { CardElement, Elements, ElementsConsumer } from '@stripe/react-stripe-js';
 import '../style/2-Card-Detailed.css';
 import Strapi from 'strapi-sdk-javascript/build/main';
 import { getCart, calculatePrice, clearCart, calculateAmount } from '../utils';
@@ -91,77 +91,59 @@ const ErrorMessage = ({children}) => (
   </div>
 );
 
-const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
-  const [error, setError] = useState(null);
-  const [cardComplete, setCardComplete] = useState(false);
-  const [processing, setProcessing] = useState(false);
-  const [paymentMethod, setPaymentMethod] = useState(null);
-  const [address, setAddress] = useState({
+
+class CheckoutForm extends React.Component {
+  state = {
+    cartItems: [],
+    email: '',
+    name: '',
+    phone: '',
     city: '',
     country: '',
     line1: '',
     postal_code: '',
     state: '',
-  });
-  const [billingDetails, setBillingDetails] = useState({
-    email: '',
-    name: '',
-    phone: '' 
-  });
-  const [cartItems, setCartItems] = useState([]);
+    error: null,
+    cardComplete: false,
+    processing: false,
+    paymentMethod: null,
 
-  useEffect(() => {
-    setInterval(() => {
-      setCartItems(getCart());
-    }, 5000);
-  });
-  
-  
+  }
+  componentDidMount() {
+    this.setState({ cartItems: getCart()});
+  }
 
-  const ResetButton = ({onClick}) => (
-    <button type="button" className="ResetButton" onClick={onClick}>
-      <svg width="32px" height="32px" viewBox="0 0 32 32">
-        <path
-          fill="#FFF"
-          d="M15,7.05492878 C10.5000495,7.55237307 7,11.3674463 7,16 C7,20.9705627 11.0294373,25 16,25 C20.9705627,25 25,20.9705627 25,16 C25,15.3627484 24.4834055,14.8461538 23.8461538,14.8461538 C23.2089022,14.8461538 22.6923077,15.3627484 22.6923077,16 C22.6923077,19.6960595 19.6960595,22.6923077 16,22.6923077 C12.3039405,22.6923077 9.30769231,19.6960595 9.30769231,16 C9.30769231,12.3039405 12.3039405,9.30769231 16,9.30769231 L16,12.0841673 C16,12.1800431 16.0275652,12.2738974 16.0794108,12.354546 C16.2287368,12.5868311 16.5380938,12.6540826 16.7703788,12.5047565 L22.3457501,8.92058924 L22.3457501,8.92058924 C22.4060014,8.88185624 22.4572275,8.83063012 22.4959605,8.7703788 C22.6452866,8.53809377 22.5780351,8.22873685 22.3457501,8.07941076 L22.3457501,8.07941076 L16.7703788,4.49524351 C16.6897301,4.44339794 16.5958758,4.41583275 16.5,4.41583275 C16.2238576,4.41583275 16,4.63969037 16,4.91583275 L16,7 L15,7 L15,7.05492878 Z M16,32 C7.163444,32 0,24.836556 0,16 C0,7.163444 7.163444,0 16,0 C24.836556,0 32,7.163444 32,16 C32,24.836556 24.836556,32 16,32 Z"
-        />
-      </svg>
-    </button>
-  );
-
-  const handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
+    const { cartItems, email, name, phone, city, country, line1, postal_code, state, error, cardComplete, processing, paymentMethod } = this.state;
+    const amount = calculateAmount(cartItems);
+
+    const { stripe, elements } = this.props;
 
     if (!stripe || !elements) {
-      // Stripe.js has not loaded yet. Make sure to disable
-      // form submission until Stripe.js has loaded.
       return;
     }
-
+    
     if (error) {
-      elements.getElement('card').focus();
+      this.props.elements.getElement('card').focus();
       return;
     }
 
     if (cardComplete) {
-      setProcessing(true);
+      this.setState({ processing: true });
+
     }
 
-    const amount = calculateAmount(cartItems);
-    console.log(amount);
-    console.log(cartItems);
-    console.log(billingDetails.email);
-    console.log(address.country);
+    
+
 
     // let token;
     // let id;
     try {
         // const cardElement = elements.getElement(CardElement);
         // id = cardElement.id;
-        // const response = await stripe.createToken(cardElement);
-        // token = response.token.id;
+        const response = await stripe.createToken(cardElement);
+        token = response.token.id;
         // const payload = await stripe.createPaymentMethod({
         // type: 'card',
         // card: elements.getElement(CardElement),
@@ -174,23 +156,24 @@ const CheckoutForm = () => {
         // });
         await strapi.createEntry('orders', {
           items: cartItems,
-          email: billingDetails.email,
+          email: email,
           amount: amount,
-          phoneNumber: billingDetails.phone,
-          country: address.country,
-          postalCode: address.postal_code,
-          state: address.state,
-          city: address.city,
-          address: address.line1,
-          name: billingDetails.name,
+          phoneNumber: phone,
+          country: country,
+          postalCode: postal_code,
+          state: state,
+          city: city,
+          address: line1,
+          name: name,
+          token
         });
-        setProcessing(false);
+        this.setState({ processing: false });
         clearCart();
         console.log('Your order has been successfully submitted!');
         // console.log(payload);
     } catch (err) {
-      setProcessing(false);
-      setError(err);
+      this.setState({ processing: false });
+      this.setState({ error: err });
       console.log(err);
     }
 
@@ -201,149 +184,133 @@ const CheckoutForm = () => {
     // }
   };
 
-  const reset = () => {
-    setError(null);
-    setProcessing(false);
-    setPaymentMethod(null);
-    setBillingDetails({
-      email: '',
-      name: '',
-      phone: ''
-    });
-    setAddress({
-      city: '',
-      country: '',
-      line1: '',
-      postal_code: '',
-      state: '',
-    });
-  };
+  render() {
+    const { cartItems, email, name, phone, city, country, line1, postal_code, state, error, cardComplete, processing, paymentMethod } = this.state;
 
-  return paymentMethod ? (
-    <div className="Result">
-      <div className="ResultTitle" role="alert">
-        Payment successful
+    return paymentMethod ? (
+      <div className="Result">
+        <div className="ResultTitle" role="alert">
+          Payment successful
+        </div>
+        <div className="ResultMessage">
+          Thanks for trying Stripe Elements. No money was charged, but we
+          generated a PaymentMethod: {paymentMethod.id}
+        </div>
       </div>
-      <div className="ResultMessage">
-        Thanks for trying Stripe Elements. No money was charged, but we
-        generated a PaymentMethod: {paymentMethod.id}
-      </div>
-      <ResetButton onClick={reset} />
-    </div>
-  ) : (
-    <form className="Form" onSubmit={handleSubmit}>
-      <fieldset className="FormGroup">
-        <Field
-          label="Name"
-          id="name"
-          type="text"
-          placeholder="Jane Doe"
-          required
-          autoComplete="name"
-          value={billingDetails.name}
-          onChange={(e) => {
-            setBillingDetails({...billingDetails, name: e.target.value});
-          }}
-        />
-        <Field
-          label="Address"
-          id="line1"
-          type="text"
-          placeholder="123 Main Street"
-          required
-          autoComplete="address"
-          value={address.line1}
-          onChange={(e) => {
-            setAddress({...address, line1: e.target.value});
-          }}
-        />
-        <Field
-          label="City"
-          id="city"
-          type="text"
-          placeholder="New York City"
-          required
-          autoComplete="city"
-          value={address.city}
-          onChange={(e) => {
-            setAddress({...address, city: e.target.value});
-          }}
-        />
-        <Field
-          label="State"
-          id="state"
-          type="text"
-          placeholder="New York"
-          required
-          autoComplete="state"
-          value={address.state}
-          onChange={(e) => {
-            setAddress({...address, state: e.target.value});
-          }}
-        />
-        <Field
-          label="Postal Code"
-          id="postalCode"
-          type="text"
-          placeholder="10013"
-          required
-          autoComplete="postalCode"
-          value={address.postalCode}
-          onChange={(e) => {
-            setAddress({...address, postal_code: e.target.value});
-          }}
-        />
-        <Field
-          label="Country"
-          id="country"
-          type="text"
-          placeholder="United States"
-          required
-          autoComplete="country"
-          value={address.country}
-          onChange={(e) => {
-            setAddress({...address, country: e.target.value});
-          }}
-        />
-        <Field
-          label="Email"
-          id="email"
-          type="email"
-          placeholder="janedoe@gmail.com"
-          required
-          autoComplete="email"
-          value={billingDetails.email}
-          onChange={(e) => {
-            setBillingDetails({...billingDetails, email: e.target.value});
-          }}
-        />
-        <Field
-          label="Phone"
-          id="phone"
-          type="tel"
-          placeholder="(941) 555-0123"
-          required
-          autoComplete="tel"
-          value={billingDetails.phone}
-          onChange={(e) => {
-            setBillingDetails({...billingDetails, phone: e.target.value});
-          }}
-        />
-      </fieldset>
-      <fieldset className="FormGroup">
-        <CardField
-          onChange={(e) => {
-            setError(e.error);
-            setCardComplete(e.complete);
-          }}
-        />
-      </fieldset>
-      {error && <ErrorMessage>{error.message}</ErrorMessage>}
-      <SubmitButton processing={processing} error={error} disabled={!stripe}>
-        Pay $25
-      </SubmitButton>
-    </form>
-  );
+    ) : (
+      <form className="Form" onSubmit={this.handleSubmit}>
+        <fieldset className="FormGroup">
+          <Field
+            label="Name"
+            id="name"
+            type="text"
+            placeholder="Jane Doe"
+            required
+            autoComplete="name"
+            value={name}
+            onChange={(e) => {
+              this.setState({ name: e.target.value });
+            }}
+          />
+          <Field
+            label="Address"
+            id="line1"
+            type="text"
+            placeholder="123 Main Street"
+            required
+            autoComplete="address"
+            value={line1}
+            onChange={(e) => {
+              this.setState({ line1: e.target.value });
+            }}
+          />
+          <Field
+            label="City"
+            id="city"
+            type="text"
+            placeholder="New York City"
+            required
+            autoComplete="city"
+            value={city}
+            onChange={(e) => {
+              this.setState({ city: e.target.value });
+            }}
+          />
+          <Field
+            label="State"
+            id="state"
+            type="text"
+            placeholder="New York"
+            required
+            autoComplete="state"
+            value={state}
+            onChange={(e) => {
+              this.setState({ state: e.target.value });
+            }}
+          />
+          <Field
+            label="Postal Code"
+            id="postalCode"
+            type="text"
+            placeholder="10013"
+            required
+            autoComplete="postalCode"
+            value={postal_code}
+            onChange={(e) => {
+              this.setState({ postal_code: e.target.value });
+            }}
+          />
+          <Field
+            label="Country"
+            id="country"
+            type="text"
+            placeholder="United States"
+            required
+            autoComplete="country"
+            value={country}
+            onChange={(e) => {
+              this.setState({ country: e.target.value });
+            }}
+          />
+          <Field
+            label="Email"
+            id="email"
+            type="email"
+            placeholder="janedoe@gmail.com"
+            required
+            autoComplete="email"
+            value={email}
+            onChange={(e) => {
+              this.setState({ email: e.target.value });
+            }}
+          />
+          <Field
+            label="Phone"
+            id="phone"
+            type="tel"
+            placeholder="(941) 555-0123"
+            required
+            autoComplete="tel"
+            value={phone}
+            onChange={(e) => {
+              this.setState({ phone: e.target.value });
+            }}
+          />
+        </fieldset>
+        <fieldset className="FormGroup">
+          <CardField
+            onChange={(e) => {
+              this.setState({ error: e.error, cardComplete: e.complete });
+            }}
+          />
+        </fieldset>
+        {error && <ErrorMessage>{error.message}</ErrorMessage>}
+        <SubmitButton processing={processing} error={error} disabled={!this.props.stripe}>
+          Pay $25
+        </SubmitButton>
+      </form>
+    )
+  }
 };
 
 const ELEMENTS_OPTIONS = {
@@ -354,18 +321,12 @@ const ELEMENTS_OPTIONS = {
   ],
 };
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_1wnyBxAYIl0XrWg7nOfLLVv900hS1OkZ8Z');
-
-const App = () => {
+export default function InjectedCheckoutForm() {
   return (
-    <div className="AppWrapper">
-      <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
-        <CheckoutForm />
-      </Elements>
-    </div>
+    <ElementsConsumer>
+      {({ stripe, elements }) => (
+        <CheckoutForm stripe={stripe} elements={elements} />
+      )}
+    </ElementsConsumer>
   );
-};
-
-export default App;
+}
--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import CheckoutForm from './CheckoutForm2';
-import App from './CardMinimal'
-// import App from './2-Split-Card';
+import CheckoutForm from './CardMinimal'
 
 
 const stripePromise = loadStripe('pk_test_1wnyBxAYIl0XrWg7nOfLLVv900hS1OkZ8Z');
@@ -11,13 +9,9 @@ const stripePromise = loadStripe('pk_test_1wnyBxAYIl0XrWg7nOfLLVv900hS1OkZ8Z');
 class Checkout extends Component {
   render() {
     return (
-      <div>
-        {/* <Elements stripe={stripePromise}>
-            <CheckoutForm />
-         </Elements> */}
-        <App />
-      </div>
-      // <App />
+      <Elements stripe={stripePromise}>
+          <CheckoutForm />
+      </Elements>
     );
   }
 }
